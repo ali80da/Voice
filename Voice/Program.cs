e6ef89e378d1b1670f4c079a43e57dcd460922ed
Ali Darehshori <Ali Darehshori@ADMINISTRATOR>
2022-02-22 18:56:13 +0330

    Add project files.

---
+++ Voice/Program.cs
@@ -0,0 +1,27 @@
+﻿// Use Voice
+using System.Speech.Synthesis;
+
+/*Console.OutputEncoding = System.Text.Encoding.Unicode;*/
+
+string? TypeAnything;
+
+SpeechSynthesizer Sound = new SpeechSynthesizer();
+Sound.SetOutputToDefaultAudioDevice();
+
+
+Console.WriteLine("START THE PROGRAM");
+Console.WriteLine("For Use   PROGRAM Please Type Anything\n");
+
+
+for (string? Say = ""; Say != null;)
+{
+    Console.Write("You Want The PROGRAM To Say: ");
+    TypeAnything = Console.ReadLine();
+    Sound.Speak(TypeAnything);
+    Console.Write("PROGRAM Say: " + TypeAnything);
+    Console.WriteLine("\n------------------------------------");
+}
+
+
+Console.WriteLine("Press Any Key To Close Program");
+Console.ReadKey();
